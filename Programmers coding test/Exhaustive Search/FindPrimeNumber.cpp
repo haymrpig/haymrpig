936397b744ed3ad78735c5e7f8510e33edfe73db
haymrpig <71866756+haymrpig@users.noreply.github.com>
2021-10-01 15:24:21 +0900

    Update FindPrimeNumber.cpp

--- Programmers coding test/Exhaustive Search/FindPrimeNumber.cpp
+++ Programmers coding test/Exhaustive Search/FindPrimeNumber.cpp
@@ -5,6 +5,16 @@
 #include <iostream>
 using namespace std;
 
+/*
+    1. string에서 vector로 숫자 하나씩 저장
+    2. 숫자 자리수만큼 배열에 prime number 찾아서 1저장
+    3. DFS를 통해 가능한 조합 모두 추출 ( 단, 0이 첫째자리일 경우 제외 ) -> 만들고자하는 자리수의 숫자가 되면 map에 string 형태로 삽입
+    4. map에 저장된 string을 int로 변경 후, prime number인지 확인 -> 맞을 경우 answer+1
+    
+    < 수정해야할 사항 >
+    - prime number 찾는 알고리즘 구현을 비효율적으로 for문을 두번 돌렸기에 수정 필요
+    - 조합을 구할 때 DFS보다 next_permutation 이용하면 편하게 구현 가능하기에 수정 필요
+*/
 void DFS(vector<char> v, vector<int>& visited,int num, int depth, string s, map<string, string>& m){
     if( depth == num ){
         m[s]=s; 
